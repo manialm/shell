3f1ce0cfeedb576e89d9855f3eb5562dbd94c8a6
Mani Alimadadi <mani.alimadadi7@gmail.com>
2022-05-03 10:14:28 +0430

    Use assert in tests

--- test/str_array_test.c
+++ test/str_array_test.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 #include <str_array.h>
+#include <assert.h>
+#include <string.h>
+
+void check(str_array *array, char *data[], size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        str_array_append(array, data[i]);
+    }
+
+    assert(array->size == n);
+
+    for (size_t i = 0; i < n; i++) {
+        size_t n = strlen(data[i]);
+        assert(strncmp(array->data[i], data[i], n) == 0);
+    }
+}
 
 int main() {
     str_array *array = str_array_new();
-    str_array_append(array, "a");
-    str_array_append(array, "b");
-    str_array_append(array, "c");
-    str_array_append(array, "d");
-    str_array_append(array, "e");
-    str_array_append(array, "f");
+    char *data[] = {"ab", "cd", "ef"};
+    size_t n = sizeof(data) / sizeof(data[0]);
+    check(array, data, n);
+
     str_array_print(array);
 
-    str_array_pop(array);
-    str_array_pop(array);
-    str_array_pop(array);
-    str_array_pop(array);
-    str_array_pop(array);
-    str_array_pop(array);
+    assert(array->size == n);
+    assert(array->data[array->size+1] == NULL);
+
+    for (size_t i = 0; i < n; i++) {
+        str_array_pop(array);
+    }
+
     str_array_print(array);
 
-    printf("\n");
-
-    str_array_append(array, "a");
-    str_array_append(array, "b");
-    str_array_append(array, "c");
-    str_array_append(array, "d");
-    str_array_append(array, "e");
-    str_array_append(array, "f");
-    str_array_print(array);
+    assert(array->size == 0);
 
     str_array_destroy(array);
 
     str_array_print(array);
-    printf("%zu\n", array->size);
-    
+    assert(array->size == 0);
+
+    printf("Tests passed!\n");
 }
